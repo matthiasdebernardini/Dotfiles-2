c637206b65cb65bec698f179bfb2c86cba524ae4
Matthias Debernardini <32387851+matthiasdebernardini@users.noreply.github.com>
2020-09-11 21:44:21 +0200

    dd

--- os/ubuntu/0_install_fish_tmux_vim.sh
+++ os/ubuntu/0_install_fish_tmux_vim.sh
@@ -16,8 +16,7 @@ sudo apt purge --auto-remove cmake
 # install CMake
 version=3.18
 build=1
-mkdir ~/temp
-cd ~/temp
+cd gits
 wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
 tar -xzvf cmake-$version.$build.tar.gz
 cd cmake-$version.$build/
